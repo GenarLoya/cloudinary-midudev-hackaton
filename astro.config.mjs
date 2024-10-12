1c33e33c76b9f0279f0aa4e6b98775d342668455
Caresle <carabesle@gmail.com>
2024-10-12 07:23:38 -0600

    feat(config): add tailwind css for fast styling Fixes #21

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import db from '@astrojs/db';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db(), tailwind()]
 });
