08d255fdcab155c1d8b95d7c30695b7d6b5fb0b0
Caresle <carabesle@gmail.com>
2024-10-12 08:30:52 -0600

    feat(service): service for send required data to DB Fixes #11

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind()]
+  integrations: [db(), tailwind()],
+  output: 'server'
 });
