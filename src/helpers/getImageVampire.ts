e8935c9b3250ab26afbbacee86c6cb90a254900b
GenarLoya <genaro2104@gmail.com>
2024-10-13 23:15:47 -0600

    feat(external-api): how works cloudinary api for replace object in images Fixes #7

---
+++ src/helpers/getImageVampire.ts
@@ -0,0 +1,24 @@
+import { getCldImageUrl } from "astro-cloudinary/helpers";
+
+export default function getImageVampire(id: string): {
+  transformedUrl: string;
+  originalUrl: string;
+} {
+  const transformedUrl = getCldImageUrl({
+    src: id,
+    replace: {
+      from: "person",
+      to: "spooky vampire",
+      preserveGeometry: true,
+    },
+  });
+
+  const originalUrl = getCldImageUrl({
+    src: id,
+  });
+
+  return {
+    transformedUrl,
+    originalUrl,
+  };
+}
