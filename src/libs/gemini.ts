006ab2d8ee36937c02b31151e3cad9222e14499c
GenarLoya <105999785+GenarLoya@users.noreply.github.com>
2024-10-17 15:47:10 +0000

    feat(external-api): calling to external api an generate history

--- src/libs/gemini.ts
+++ src/libs/gemini.ts
@@ -1,5 +1,14 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(import.meta.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(import.meta.env.GEMINI_API_KEY);
 
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: {
+    temperature: 1
+},
+safetySettings: [
+    {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    }
+]
+});
