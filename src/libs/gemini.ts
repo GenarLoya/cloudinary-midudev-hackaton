a755a95e3c119ea7eabaaddcf8559fc3354dd5d1
GenarLoya <genaro2104@gmail.com>
2024-10-17 08:36:22 -0600

    feat(external-api): adding to view

--- src/libs/gemini.ts
+++ src/libs/gemini.ts
@@ -1,5 +1,5 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(import.meta.GEMINI_API_KEY);
 
 export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
